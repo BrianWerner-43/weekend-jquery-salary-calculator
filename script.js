2fefc11cd384736cc9dd1abe8666b80dba066429
Brian Werner <brianmw199@gmail.com>
2023-05-07 00:23:03 -0500

    Created an empty array that will push objects(employee info) into created a function that will append employee info to the DOM

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-function document (onReady) {
+$(document).ready(onReady);
 
+// created an empty array to push new employee info in
+let newEmployeeInfo = [];
+
+function onReady() {
+// Put in button clicks for the submit & delete also put in an event 
+//for calculating  total monthly salary
+
+  $('#submit-button').on('click', employeeInfo);
+  $('tbody').on('click', '.deleteButton', removeInfo);
+  //$('#totalMonthly').text(totalMonthlySalary);
 }
 
+function newEmployee() {
+  let newEmployee = {
+    firstName: $('#submitFirstname').val(),
+    lastName: $('#submitLastname').val(),
+    iD: $('#submitId').val(),
+    jobTitle: $('#submitJobTitle').val(),
+    annualSalary: $('#submitAnnualSalary').val()
+  }
+};
+// Setting up to push employee info into the array
+newEmployeeInfo.push(newEmployee);
 
-// function employeeInfo (event) {
-//     let affirmationInput=$('#affirmationInput').val();
-//     let authorInput=$('#authorInput').val();
-    
 //     event.preventDefault();
 //     $('tbody').append(`
 //       <tr>
-//       <td>${affirmationInput}</td>
-//       <td>${authorInput}</td>
-//       <td><button class="deleteMe">❌</button></td>
+//       <td>${submitFirstname}</td>
+//       <td>${submitLastname}</td>
+//       <td><button class="deleteButton">Delete</button></td>
 //       </tr>
+// `)
+// }
+function removeInfo() {
+  console.log('remove info')
+  $(this).parent().parent().remove();
+}
