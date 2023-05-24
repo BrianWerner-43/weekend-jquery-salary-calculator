b66dd10a3fe9b37e6137d3273f89c2f85b05d855
Brian Werner <brianmw199@gmail.com>
2023-05-23 22:53:01 -0500

    Did an update to the removeInfo function to have my on click delete button delete the row

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 $(document).ready(onReady);
+let salary = 0;
+let totalSalary = 0;
 
 // created an empty array to push new employee info in
 let newEmployeeInfo = [];
+// let currentAnnualCost = 0;
 
 function onReady() {
-// Put in button clicks for the submit & delete also put in an event 
-//for calculating  total monthly salary
 
+
+// Event listeners
   $('#submit-button').on('click', newEmployee);
-  $('tbody').on('click', '.deleteButton', removeInfo);
- // $('#monthlyCost').text(annualSalary);
+  $('#employeeInfo').on('click', '.deleteButton', removeInfo);
+  
 }
 
+// Event handler
 function newEmployee(event) {
+  event.preventDefault() 
+  
+
   console.log('it clicked!');
+  
   let newEmployee = {
    firstName: $('#submitFirstname').val(),
     lastName: $('#submitLastname').val(),
     iD: $('#submitId').val(),
     jobTitle: $('#submitJobtitle').val(),
     annualSalary: $('#submitAnnualSalary').val()
+  
   }
+  
   console.log(newEmployee);
 
-// pushed employee info into the array
+
 
 newEmployeeInfo.push(newEmployee);
-// My preventDefault won't append anything to the DOM
+
+// Got it on the DOM
 console.log('New employee info');
-    event.preventDefault() //Console says it can't read prperties
+
+    
     $('tbody').append(`
+    
       <tr>
       <td>${newEmployee.firstName}</td>
       <td>${newEmployee.lastName}</td>
@@ -38,44 +51,45 @@ console.log('New employee info');
       <td>${newEmployee.annualSalary}</td>
       <td><button class="deleteButton">Delete</button></td>
       </tr>
+    
 `)
-console.log('append employee info to the DOM');
-$('input').val('');
-  }
+
+$('#monthly-total').text(annual_salary);
+
+
+// console.log('append employee info to the DOM');
+ $('.input').val('');
+}
 
   // crerate a function to calculate employee annual salary
-  function annualSalary() {
-    let annualSalary = 0;
-    for (let i=0; i < newEmployeeInfo.length; i++) {
-      annualSalary += newEmployeeInfo[i].annualSalary
+  function annual_salary() {
+    salary = Number($('#submitAnnualSalary').val());
+    totalSalary += salary;
+    console.log(salary);
+    let monthlyTotal = totalSalary/12;
+    totalCost(monthlyTotal);
+    return monthlyTotal;
     
-    }
   }
 console.log('Total annual salary')
 
-// creating 2 variables to calculate annual salary/months
-// to get monthly cost
-let months = 12;
-let monthlyCost = annualSalary / 12;
 
-function totalCost(monthlyCost) {
+function totalCost(monthly) {
 
-annualSalary += Math.round(annualSalary / 12);
-  if ( monthlyCost < 20000) {
-    $('#totalMonthly').css( background-color, 'red');
-  }else if (monthlyCost > 20000) {
-    $('#totalMonthly').css( background-color, 'gray');
+
+  if ( monthly > 20000) {
+    $('#monthly-total').css( 'background-color', 'red');
+  }else if (monthly > 20000) {
+
+   $('#monthly-total').css( background-color, 'gray');
   }
-};
+}
 
- //console.log('should turn red',totalCost);
 
+
+// The delete function for the delete button.'
 function removeInfo(){
     console.log('deleted')
-    $(this).parent().parent().remove();
+    $(this).closest('tr').remove(); 
+}
 
-}
-function newEmployee1() {
-  console.log('working?')
-  $(this).parent().parent().remove();
-}
